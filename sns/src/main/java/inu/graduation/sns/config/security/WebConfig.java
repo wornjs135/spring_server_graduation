8ae0b85559de316490857e89fd9c5cafe8cf3ebc
wornjs135 <rjeowornjs@naver.com>
2021-07-22 13:12:08 +0900

    cors 설정

--- sns/src/main/java/inu/graduation/sns/config/security/WebConfig.java
+++ sns/src/main/java/inu/graduation/sns/config/security/WebConfig.java
@@ -3,6 +3,7 @@ package inu.graduation.sns.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+//                .allowCredentials(true) // 특정 오리진 일때만 true
+                .maxAge(3600);
     }
 }
